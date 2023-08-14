3477ac59186366aad50c3624df9aa56e7de796fb
tonito98 <86890715+tonito98@users.noreply.github.com>
2023-08-15 01:21:45 +0200

    deploy to heroku

---
+++ client/build/precache-manifest.1d1823188dbd32bd2fc47af068bfca7b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4341ad0e1c807ef6c867785a50de3f58",
+    "url": "/index.html"
+  },
+  {
+    "revision": "26a7aaf1a454363012fd",
+    "url": "/static/css/main.1afbbfc4.chunk.css"
+  },
+  {
+    "revision": "1cf83f2a72a3371b3e83",
+    "url": "/static/js/2.0ca1094b.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.0ca1094b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26a7aaf1a454363012fd",
+    "url": "/static/js/main.378c2045.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.378c2045.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d52f44d6fb82598fed4",
+    "url": "/static/js/runtime-main.c62b8046.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
